ba4ef0211390b1c7398ea5990abeb2d59bc4ec4c
matteo <matteoezio@me.com>
2020-11-27 18:02:49 +0100

    il carosello prende forma

---
+++ main.js
@@ -0,0 +1,32 @@
+// Descrizione:
+// Creare uno slider di immagini
+// Potete usare le immagini che desiderate.
+// Per facilitarvi la vita usate immagini delle stesse dimensioni :)
+// Lo slider prevederà due frecce (icone) che permettono di scorrere tra le immagini dello slider
+// In oltre per scorrere le immagini permettere anche l’uso delle frecce sinistra e destra della tastiera
+// Utiliziamo una classe first e last  per capire quali sono la prima e ultima immagine dello slider
+// Utilizziamo una classe active per aiutarci a capire quale è l’immagine attuale da visualizzare nello slider
+// Bonus:
+// Clicchiamo sui pallini e mostriamo l’immagine corrispondente
+// Generiamo i pallini con JS
+// Consiglio del giorno:
+// Come sempre è importante usare la documentazione di jQuery per scoprire/rinfrescare memoria sulle funzioni che potrebbero esserci utili.
+
+var leftArrow = $('.prev i');
+leftArrow.click(function() {
+    var activeImg = $('img.active, i.active');
+    activeImg.prev().addClass('active');
+    activeImg.removeClass('active');
+
+    if (acvtiveImg) {
+        
+    }
+    
+});
+
+var rightArrow = $('.next i');
+rightArrow.click(function() {
+       var activeImg = $('img.active, i.active');
+    activeImg.next().addClass('active');
+    activeImg.removeClass('active');
+});
